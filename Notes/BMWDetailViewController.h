3a0120d3b4ab0fb7b642f90d567e122db853009e
Benjamin Wang <benwang@hntvlan569.1600.wlan.wireless-resnet.upenn.edu>
2013-03-10 01:39:13 -0500

    initial commit with basic map stuff

--- Notes/BMWDetailViewController.h
+++ Notes/BMWDetailViewController.h
@@ -7,10 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface BMWDetailViewController : UIViewController <UISplitViewControllerDelegate>
+@interface BMWDetailViewController : UIViewController <UISplitViewControllerDelegate, MKMapViewDelegate, CLLocationManagerDelegate>
 
 @property (strong, nonatomic) id detailItem;
 
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
 @end
--- Notes/BMWDetailViewController.m
+++ Notes/BMWDetailViewController.m
@@ -8,12 +8,15 @@
 
 #import "BMWDetailViewController.h"
 
+
 @interface BMWDetailViewController ()
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 - (void)configureView;
 @end
 
 @implementation BMWDetailViewController
+
 
 #pragma mark - Managing the detail item
 
@@ -38,6 +41,9 @@
     if (self.detailItem) {
         self.detailDescriptionLabel.text = [self.detailItem description];
     }
+    
+    
+    
 }
 
 - (void)viewDidLoad
@@ -45,6 +51,23 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self configureView];
+    
+    
+    //Setting up location
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = 100.0;
+    self.locationManager.distanceFilter = 1000.0;
+    [self.locationManager startMonitoringSignificantLocationChanges];
+    
+
+    //Setting up map
+    self.mapView.delegate = self;
+    self.mapView.mapType = MKMapTypeStandard;
+
+    //Update once, add pin to map, then stop monitoring location changes
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -57,7 +80,7 @@
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
 {
-    barButtonItem.title = NSLocalizedString(@"Master", @"Master");
+    barButtonItem.title = NSLocalizedString(@"Notes", @"Notes");
     [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
     self.masterPopoverController = popoverController;
 }
@@ -67,6 +90,43 @@
     // Called when the view is shown again in the split view, invalidating the button and popover controller.
     [self.navigationItem setLeftBarButtonItem:nil animated:YES];
     self.masterPopoverController = nil;
+}
+
+
+#pragma mark - MapView Methods
+
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:
+(NSArray *) locations
+{
+    CLLocation *location = [locations lastObject];
+    NSLog(@"lat: %f, lon: %f", location.coordinate.latitude, location.coordinate.longitude);
+    [self addPinToMapAtLocation:location];
+}
+
+- (void) addPinToMapAtLocation:(CLLocation *) location
+{
+    //this is the red pin
+    MKPointAnnotation *pin = [[MKPointAnnotation alloc] init];
+    pin.coordinate = location.coordinate;
+    
+    //Labels, with latitude/longitude markings to two decimal places
+    pin.title = @"Note created here";
+    float lat = location.coordinate.latitude;
+    float lon = location.coordinate.longitude;
+    pin.subtitle = [NSString stringWithFormat: @"lat: ~%f, lon: ~%f", lat, lon];
+    
+    //Add pin to map, puts center of screen at the pin
+    [self.mapView addAnnotation:pin];
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.01;
+    span.longitudeDelta = 0.01;
+    CLLocationCoordinate2D newCenter;
+    newCenter.latitude = location.coordinate.latitude;
+    newCenter.longitude = location.coordinate.longitude;
+    region.span = span;
+    region.center = newCenter;
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
